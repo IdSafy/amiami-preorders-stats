b317195ef32ba9d769abc859d3b981f97c4400ac
IdSafy <idsaf@outlook.com>
2024-04-20 18:16:38 +0500

    feat: add categorization

--- src/script.py
+++ src/script.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from collections import defaultdict
 from datetime import date
 from functools import reduce
@@ -29,6 +30,23 @@ def get_orders(login: str, password: str) -> list[amiami_api.ApiOrderInfo]:
 
     orders = api.get_orders_info()
     return orders
+
+
+def classify_items(
+    items: list[amiami_api.ApiItem],
+) -> dict[str, list[amiami_api.ApiItem]]:
+    categories_buckets: dict[str, list[amiami_api.ApiItem]] = defaultdict(list)
+    for item in items:
+        if item.sname.lower().find("nendoroid") != -1:
+            categories_buckets["nendoroid"].append(item)
+            continue
+        scale_match = re.search(r"\d\/\d", item.sname)
+        if scale_match is not None:
+            categories_buckets[f"{scale_match.group()} scale"].append(item)
+            continue
+        categories_buckets["other"].append(item)
+        continue
+    return categories_buckets
 
 
 @click.command()
@@ -73,7 +91,7 @@ def stats(file: str):
 
     print("------\n\n")
 
-    total_cost = 0
+    all_items: list[amiami_api.ApiItem] = []
     print("Detailed stats:")
     for month, month_orders in sorted(orders_by_month.items(), key=lambda i: i[0]):
         for order in month_orders:
@@ -81,10 +99,21 @@ def stats(file: str):
                 print(
                     f"{month}:{item.ds_no}:{order.d_no}:{item.price:>9.2f} yen:{item.price * 0.0066:>7.2f} usd: {item.sname}"
                 )
-                total_cost += item.price
+                all_items.append(item)
         print("------")
 
+    total_cost = reduce(lambda a, b: a + b.price, all_items, 0)
+    total_n_items = len(all_items)
+
     print("\n")
+    print("Categories:")
+    categories = classify_items(all_items)
+    for category, items in sorted(categories.items(), key=lambda t: t[0]):
+        print(f"{category}: {len(items)}")
+    print("------")
+
+    print("\n")
+    print(f"Total items: {total_n_items}")
     print(f"Summary cost: {total_cost:>9.2f} yen or {total_cost * 0.0066:>7.2f} usd ")
 
 
