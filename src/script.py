4bf5f486250b7443b2ea3886ac7a09082f7066f1
IdSafy <idsaf@outlook.com>
2024-04-26 20:28:49 +0500

    chore: refactor a bit to allow data export in future

--- src/script.py
+++ src/script.py
@@ -2,16 +2,15 @@ import json
 import logging
 import re
 import sys
-from collections import defaultdict
-from datetime import date
 from functools import reduce, wraps
+from itertools import groupby
 from os import environ
 from typing import TextIO
 
 import click
 from dotenv import load_dotenv
 from print_color import print
-from pydantic import TypeAdapter
+from pydantic import JsonValue, TypeAdapter
 from pydantic.json import pydantic_encoder
 
 import amiami_api
@@ -43,43 +42,61 @@ def get_orders(login: str, password: str) -> list[amiami_api.ApiOrderInfo]:
     return orders
 
 
-def classify_items(
-    items: list[amiami_api.ApiItem],
-) -> dict[str, list[amiami_api.ApiItem]]:
-    categories_buckets: dict[str, list[amiami_api.ApiItem]] = defaultdict(list)
-    for item in items:
-        if item.sname.lower().find("nendoroid") != -1:
-            categories_buckets["nendoroid"].append(item)
-            continue
-        scale_match = re.search(r"\d\/\d", item.sname)
-        if scale_match is not None:
-            categories_buckets[f"{scale_match.group()} scale"].append(item)
-            continue
-        categories_buckets["other"].append(item)
-        continue
-    return categories_buckets
+ItemWithRelatedData = tuple[
+    amiami_api.ApiItem, amiami_api.ApiOrderInfo, dict[str, JsonValue]
+]
+ItemsTable = list[ItemWithRelatedData]
+
+
+def flatten_items(orders: list[amiami_api.ApiOrderInfo]) -> ItemsTable:
+    flat_items: ItemsTable = []
+    for order in orders:
+        for item in order.items:
+            flat_items.append((item, order, {}))
+    return flat_items
+
+
+def classify_item(item: amiami_api.ApiItem) -> str:
+    if item.sname.lower().find("nendoroid") != -1:
+        return "nendoroid"
+    scale_match = re.search(r"\d\/\d", item.sname)
+    if scale_match is not None:
+        return f"{scale_match.group()} scale"
+    return "other"
+
+
+def classify_items(items: ItemsTable) -> None:
+    for item, order, metadata in items:
+        metadata["category"] = classify_item(item)
+
+
+def prepare_items_table(orders: list[amiami_api.ApiOrderInfo]) -> ItemsTable:
+    item_table = flatten_items(orders)
+    classify_items(item_table)
+    return item_table
 
 
 def print_stats(stream: TextIO, orders: list[amiami_api.ApiOrderInfo]) -> None:
-    orders_by_month: dict[date, list[amiami_api.ApiOrderInfo]] = defaultdict(list)
-    for order in orders:
-        orders_by_month[order.scheduled_release].append(order)
-
     @wraps(print)
     def _print(*args, **kwargs):
         kwargs.setdefault("file", stream)
         kwargs.setdefault("end", "")
         print(*args, **kwargs)
 
+    orders_by_month = groupby(
+        sorted(orders, key=lambda order: order.scheduled_release),
+        key=lambda order: order.scheduled_release,
+    )
     _print("By month stats:\n")
-    for month, month_orders in sorted(orders_by_month.items(), key=lambda i: i[0]):
+    for month, month_orders_iterator in orders_by_month:
+        month_orders = list(month_orders_iterator)
         cost = reduce(lambda a, b: a + b.total, month_orders, 0)
         n_items = reduce(lambda a, b: a + len(b.items), month_orders, 0)
         month_str = month.strftime("%Y-%m")
         _print(f"{month_str};{n_items:2} items;{cost:>9.2f}¥ /{cost * 0.0066:>7.2f}$\n")
     _print("------\n------\n\n")
 
-    all_items: list[amiami_api.ApiItem] = []
+    items_table = prepare_items_table(orders)
     _print("Detailed stats:\n")
     order_color_cycle = [
         "blue",
@@ -93,45 +110,47 @@ def print_stats(stream: TextIO, orders: list[amiami_api.ApiOrderInfo]) -> None:
     previous_month = None
     order_color_index = -1
     month_color_index = 2
-    for month, month_orders in sorted(orders_by_month.items(), key=lambda i: i[0]):
-        for order in month_orders:
-            for item in order.items:
-                in_stock_text, in_stock_text_color = (
-                    ("in stock", "green")
-                    if item.stock_flg == item.amount
-                    else ("not in stock", "white")
-                )
+    for item, order, metadata in sorted(
+        items_table, key=lambda pair: (pair[1].scheduled_release, pair[0].price)
+    ):
+        month = order.scheduled_release
 
-                if previous_item_order != order.d_no:
-                    order_color_index = (order_color_index + 1) % len(order_color_cycle)
-                    previous_item_order = order.d_no
-                order_color = order_color_cycle[order_color_index]
+        in_stock_text, in_stock_text_color = (
+            ("in stock", "green") if item.stock_flg == item.amount else ("N/A", "white")
+        )
 
-                month_str = month.strftime("%Y-%m")
-                if previous_month != month_str:
-                    month_color_index = (month_color_index + 1) % len(order_color_cycle)
-                    previous_month = month_str
-                month_color = order_color_cycle[month_color_index]
+        if previous_item_order != order.d_no:
+            order_color_index = (order_color_index + 1) % len(order_color_cycle)
+            previous_item_order = order.d_no
+        order_color = order_color_cycle[order_color_index]
 
-                _print(f"{month_str};", color=month_color)
-                _print(" order ")
-                _print(f"{link(order.page_link, order.d_no)}; ", color=order_color)
-                _print(f"{in_stock_text:>12};", color=in_stock_text_color)
-                _print(
-                    f"{item.price:>9.2f}¥ /{item.price * 0.0066:>7.2f}$; {link(item.page_link, item.sname)}\n"
-                )
-                all_items.append(item)
-    _print("------\n")
-    _print("------\n\n")
+        month_str = month.strftime("%Y-%m")
+        if previous_month != month_str:
+            month_color_index = (month_color_index + 1) % len(order_color_cycle)
+            previous_month = month_str
+        month_color = order_color_cycle[month_color_index]
+
+        _print(f"{month_str};", color=month_color)
+        _print(" order ")
+        _print(f"{link(order.page_link, order.d_no)}; ", color=order_color)
+        _print(f"{in_stock_text:>8};", color=in_stock_text_color)
+        _print(
+            f"{item.price:>9.2f}¥ /{item.price * 0.0066:>7.2f}$; {link(item.page_link, item.sname)}\n"
+        )
+    _print("------\n------\n\n")
 
     print("Categories:", file=stream)
-    total_cost = reduce(lambda a, b: a + b.price, all_items, 0)
-    total_n_items = len(all_items)
-    categories = classify_items(all_items)
-    for category, items in sorted(categories.items(), key=lambda t: t[0]):
+    items_by_category = groupby(
+        sorted(items_table, key=lambda t: t[2].get("category")),  # type: ignore
+        key=lambda t: t[2].get("category"),
+    )
+    for category, items_iterator in items_by_category:
+        items = list(items_iterator)
         _print(f"{category}: {len(items)}\n")
     _print("------\n------\n\n")
 
+    total_cost = reduce(lambda a, t: a + t[0].price, items_table, 0)
+    total_n_items = len(items_table)
     _print(f"Total items: {total_n_items}\n")
     _print(f"Summary cost: {total_cost:>9.2f}¥ / {total_cost * 0.0066:>7.2f}$\n")
 
