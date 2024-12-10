eee942e30dc2e1c58a100cd41e3c216cadca27bc
IdSafy <idsaf@outlook.com>
2024-12-10 18:44:34 +0500

    chore: add proper title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'AmiAmi orders tracker';
+      return args;
+    });
+  },
 })
