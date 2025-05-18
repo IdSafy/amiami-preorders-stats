46ec6e167ccffb00bec5130e296bcf1eaeb96909
IdSafy <idsaf@outlook.com>
2025-05-18 18:59:19 +0500

    fix: config missing fields

--- src/amiami_api/config.py
+++ src/amiami_api/config.py
@@ -8,6 +8,7 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=False, env_file=".env", env_prefix="AMIAMI_", extra="ignore")
 
     username: str = Field(alias="AMIAMI_LOGIN")
-    password: str
+    password: str = Field(alias="AMIAMI_PASSWORD")
     store_file_path: Path = Field(default=Path("./orders.json"))
     telegram_bot_token: str = Field(alias="TELEGRAM_BOT_TOKEN")
+    telegram_bot_white_list: list[str] = Field(default_factory=list, alias="TELEGRAM_BOT_WHITE_LIST")
